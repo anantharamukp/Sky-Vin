1fba2b1a84fc78f46af5ae3efa5a80d15fd35ba2
dstamil <99588972+dstamil@users.noreply.github.com>
2022-12-26 12:41:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dsbro:dsbro@cluster0.gtu48kz.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "dsbro")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
