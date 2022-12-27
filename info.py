f2555565ad845c6d29465fa3031f1fc85c9655fd
Dsdstam <120117100+Dsdstam@users.noreply.github.com>
2022-12-27 07:51:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dsbro:dsbro@cluster0.gtu48kz.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dstamil:dstamil@cluster0.kbmufno.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "dsbro")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
